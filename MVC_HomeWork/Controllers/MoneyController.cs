8b406c55a7688b4c0fa92d386290299c600f0bf4
Eric Huang <hongyuan287@gmail.com>
2018-07-16 22:53:29 +0800

    MVC_HomeWork_Day1

---
+++ MVC_HomeWork/Controllers/MoneyController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_HomeWork.Models.ViewModels;
+
+namespace MVC_HomeWork.Controllers
+{
+    public class MoneyController : Controller
+    {
+        // GET: Money
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult MoneyDetail()
+        {
+            return View(GetMoneyDetail());
+        }
+
+        public IEnumerable<MoneyViewModel> GetMoneyDetail()
+        {
+            for (int x = 1; x <= 50; x++)
+            {
+                yield return new MoneyViewModel()
+                {
+                    Category = (x % 2 == 0) ? CategoryEnum.收入 : CategoryEnum.支出,
+                    Amount = x * 1000 + 50 - (x * 100),
+                    Date = DateTime.Now.AddDays(-x)
+                };
+            }
+        }
+    }
+}
---
+++ MVC_HomeWork/Models/ViewModels/MoneyViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MVC_HomeWork.Models.ViewModels
+{
+    public enum CategoryEnum
+    {
+        收入 = 'I',
+        支出 = 'E'
+    }
+
+    public class MoneyViewModel
+    {
+        [DisplayName("類別")]
+        public CategoryEnum Category { get; set; }
+        [DisplayName("金額")]
+        public int Amount { get; set; }
+        [DisplayName("日期")]
+        public DateTime Date { get; set; }
+        [DisplayName("備註")]
+        public string Description { get; set; }
+
+    }
+
+    
+}
